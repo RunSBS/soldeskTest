fb9577656a81c618e5a2fb18813dd08fa815acac
Hyun <dacapo629@naver.com>
2025-12-23 15:54:57 +0900

    스프링부트 프로젝트 세팅완료

---
+++ src/main/java/jen/soldesk/SoldeskApplication.java
@@ -0,0 +1,13 @@
+package jen.soldesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoldeskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoldeskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jen/soldesk/SoldeskApplicationTests.java
@@ -0,0 +1,13 @@
+package jen.soldesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoldeskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
